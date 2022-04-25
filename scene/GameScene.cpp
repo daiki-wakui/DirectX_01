dd447eb70d41ffd4383c21c6e4b116ea4d910e10
DaikiWakui <k021g1119@g.neec.ac.jp>
2022-04-25 15:00:23 +0900

    固定カメラ

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -7,7 +7,6 @@ using namespace DirectX;
 GameScene::GameScene() {}
 
 GameScene::~GameScene() { 
-	delete sprite_;
 	delete model_;
 }
 
@@ -21,53 +20,132 @@ void GameScene::Initialize() {
 	//ファイル名を指定してテクスチャを読み込む
 	textureHandle_ = TextureManager::Load("mario.jpg");
 
-	//スプライトの生成
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-
 	//3Dモデルの生成
 	model_ = Model::Create();
 
-	//サウンドデータの読み込み
-	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
+	//x,y,z方向のスケーリング
+	worldTransfrom_.scale_ = {1.0f, 1.0f, 1.0f};
+
+	//x,y,z軸周りの回転角を設定
+	worldTransfrom_.rotation_ = {0.0f, 0.0f, 0.0f};
+
+	//z,y,z軸周りの平行移動を設定
+	worldTransfrom_.translation_ = {0.0f, 0.0f, 0.0f};
+
+	//カメラ視点座標を設定
+	viewProjection_.eye = {0, 15, -20};
+
+	//カメラ注視点座標を設定
+	viewProjection_.target = {0, 0, 0};
+
+	//viewProjection_.up = {0.0f, 0.0f, 0.0f};
+
 
 	//ワールドトランスフォームの初期化
 	worldTransfrom_.Initialize();
+
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
-
-	worldTransfrom_.scale_ = {5.0f, 5.0f, 5.0f};
-	//worldTransfrom_.rotation_ = {0.0f, XM_PI / 4.0f, 0.0f};
-
-	worldTransfrom_.translation_ = {10.0f, 10.0f, 10.0f};
-	worldTransfrom_.rotation_ = {XMConvertToRadians(45.0f), XMConvertToRadians(45.0f), 0.0f};
-	worldTransfrom_.Initialize();
 }
 
-void GameScene::Update() { 
-	//スプライトの今の座標を所得
-	XMFLOAT2 position = sprite_->GetPosition();
+void GameScene::Update() {
+	//正面ベクトルの長さ
+	XMFLOAT3 length = {0, 0, 1};
 
-	//座標を{2,0}移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
+	//正面ベクトル
+	XMFLOAT3 frontVec = {0, 0, 0};
 
-	//デバッグテキストの表示
-	//debugText_->Print("Kaizokuou ni oreha naru.", 50, 50, 1.0f);
+	//始点
+	XMFLOAT3 start = {worldTransfrom_.translation_};
 
-	//書式指定付き表示
-	//debugText_->SetPos(50, 70);
-	//debugText_->Printf("year:%d", 2022);
+	//終点
+	XMFLOAT3 end = {0, 0, 0};
 
-	//変数の値をインクルメント
-	value_++;
+	//オブジェクトの移動ベクトル
+	XMFLOAT3 move = {0, 0, 0};
 
-	//値を含んだ文字列
-	std::string strDebug = std::string("Value:") + std::to_string(value_);
+	//オブジェクトの回転ベクトル
+	XMFLOAT3 rotaMove = {0, 0, 0};
 
-	//デバッグテキストの表示
-	//debugText_->Print(strDebug, 50, 50, 1.0f);
+	//オブジェクトの移動スピード
+	const float obSpeed = 0.2f;
+
+	//オブジェクトの回転スピード
+	const float rotaSpeed = 0.05f;
+
+	//オブジェクトの前進と後退
+	if (input_->PushKey(DIK_UP)) {
+		move = {
+			sinf(worldTransfrom_.rotation_.y)*obSpeed,
+			0,
+			cosf(worldTransfrom_.rotation_.y)*obSpeed
+		};
+	} else if (input_->PushKey(DIK_DOWN)) {
+		move = {
+			sinf(worldTransfrom_.rotation_.y) * -obSpeed,
+			0,
+			cosf(worldTransfrom_.rotation_.y) * - obSpeed
+		};
+	}
+	//オブジェクトの回転
+	if (input_->PushKey(DIK_RIGHT)) {
+		rotaMove = {0, rotaSpeed, 0};
+	} else if (input_->PushKey(DIK_LEFT)) {
+		rotaMove = {0, -rotaSpeed, 0};
+	}
+
+	//ベクトルの加算
+	worldTransfrom_.translation_.x += move.x;
+	worldTransfrom_.translation_.z += move.z;
+
+	//回転
+	worldTransfrom_.rotation_.y += rotaMove.y;
+
+	end.x = start.x + length.x;
+	end.y = start.y + length.y;
+	end.z = start.z + length.z;
+
+	end.x = sinf(worldTransfrom_.rotation_.y);
+	end.z = cosf(worldTransfrom_.rotation_.y);
+	
+	/*frontVec.x = end.x - start.x;
+	frontVec.y = end.y - start.y;
+	frontVec.z = end.z - start.z;
+
+	frontVec.x /= length.x;
+	frontVec.y /= length.y;
+	frontVec.z /= length.z;
+	
+	start.x += frontVec.x;
+	start.y += frontVec.x;
+	start.z += frontVec.z;*/
+
+	//再計算
+	worldTransfrom_.UpdateMatrix();
+
+	//デバッグ
+	debugText_->SetPos(0, 0);
+	debugText_->Printf(
+	  "translation_:(%f,%f,%f)", 
+		worldTransfrom_.translation_.x,
+		worldTransfrom_.translation_.y,
+		worldTransfrom_.translation_.z);
+
+	debugText_->SetPos(0, 20);
+	debugText_->Printf(
+	  "rotation_:(%f,%f,%f)",
+		worldTransfrom_.rotation_.x,
+		worldTransfrom_.rotation_.y,
+		worldTransfrom_.rotation_.z);
+
+	debugText_->SetPos(0, 40);
+	debugText_->Printf("start:(%f,%f,%f)",start.x,start.y,start.z);
+
+	debugText_->SetPos(0, 60);
+	debugText_->Printf("end:(%f,%f,%f)", end.x, end.y, end.z);
+
+	debugText_->SetPos(0, 80);
+	debugText_->Printf("frontVec:(%f,%f,%f)", frontVec.x, frontVec.y, frontVec.z);
 }
 
 void GameScene::Draw() {
@@ -97,7 +175,9 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	
 	model_->Draw(worldTransfrom_, viewProjection_, textureHandle_);
+
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -110,7 +190,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	//sprite_->Draw();
 
 
 	// デバッグテキストの描画
